6278cc20607f606bcc0a96b7858cec8b892bf8f1
Franz <fernandofranquiz@gmail.com>
2023-11-16 00:44:42 +0100

    modification in list component

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -3,16 +3,27 @@ import { useEffect } from 'react';
 import { Card } from '../card/card';
 
 import { useCharacters } from '../../hooks/use.characters';
-/* import { useSelector } from 'react-redux';
-import { RootState } from '../../store/store'; */
+
+import { RootState } from '../../store/store';
+import { useSelector } from 'react-redux';
 
 export function List() {
   const { loadCharacters, characters } = useCharacters();
-  /*   const { charactersState } = useSelector((state: RootState) => state.characterState);
-   */
+  const { charactersState } = useSelector(
+    (state: RootState) => state.characterState
+  );
+
   useEffect(() => {
     loadCharacters();
   }, [loadCharacters]);
+
+  if (charactersState === 'loading') {
+    return <p className="loading"> LOADING </p>;
+  }
+
+  if (charactersState === 'error') {
+    return <p> Sorry bro</p>;
+  }
 
   return (
     <>
